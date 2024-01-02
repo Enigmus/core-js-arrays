32a994fafcdf71ded41a66ef2b02eba35cf059a3
sd <sd_net@mail.ru>
2024-01-02 15:13:39 +0300

    fix&feat: fix fn getTail()& feat fn doubleArray()

--- src/arrays-tasks.js
+++ src/arrays-tasks.js
@@ -211,7 +211,7 @@ function getHead(arr, n) {
  *    getTail([ 'a', 'b', 'c', 'd'], 0) => []
  */
 function getTail(arr, n) {
-  return arr.slice(-n);
+  return n ? arr.slice(-n) : [];
 }
 
 /**
@@ -226,8 +226,8 @@ function getTail(arr, n) {
  *    doubleArray([0, 1, 2, 3, 4, 5]) => [0, 1, 2, 3, 4, 5, 0, 1, 2, 3, 4, 5]
  *    doubleArray([]) => []
  */
-function doubleArray(/* arr */) {
-  throw new Error('Not implemented');
+function doubleArray(arr) {
+  return arr.concat(arr);
 }
 
 /**
