344849c125bbd55f0cef5a5f99dbdd80a32d9b7e
sd <sd_net@mail.ru>
2024-01-02 21:58:40 +0300

    feat: add fn calculateBalance();

--- src/arrays-tasks.js
+++ src/arrays-tasks.js
@@ -329,8 +329,11 @@ function selectMany(/* arr, childrenSelector */) {
  *   calculateBalance([ [ 10, 8 ], [ 1, 5 ] ])  => (10 - 8) + (1 - 5) = 2 + -4 = -2
  *   calculateBalance([]) => 0
  */
-function calculateBalance(/* arr */) {
-  throw new Error('Not implemented');
+function calculateBalance(arr) {
+  return arr.flat(Infinity).reduce((acc, el, i) => {
+    if (i % 2) return acc - el;
+    return acc + el;
+  }, 0);
 }
 
 /**
